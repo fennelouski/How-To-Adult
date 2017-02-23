bcb3900a69bf7dcec6805969e90dbbf5555017ab
Nathan Fennel <nathanfennel@gmail.com>
2017-02-23 15:38:38 -0500

    Added collection view cell for the main vc

---
+++ How To Adult/HTACollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  HTACollectionViewCell.m
+//  How To Adult
+//
+//  Created by Nathan Fennel on 2/23/17.
+//  Copyright © 2017 Nathan Fennel. All rights reserved.
+//
+
+#import "HTACollectionViewCell.h"
+
+@implementation HTACollectionViewCell
+
+@end

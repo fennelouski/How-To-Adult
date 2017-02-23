89f6a457f595e761cbefe66bbfef5f66a51bcac7
Nathan Fennel <nathanfennel@gmail.com>
2017-02-23 15:54:42 -0500

    Added collection view layout for the main vc

---
+++ How To Adult/HTACollectionViewLayout.h
@@ -0,0 +1,13 @@
+//
+//  HTACollectionViewLayout.h
+//  How To Adult
+//
+//  Created by Nathan Fennel on 2/23/17.
+//  Copyright © 2017 Nathan Fennel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HTACollectionViewLayout : UICollectionViewLayout
+
+@end
---
+++ How To Adult/HTACollectionViewLayout.m
@@ -0,0 +1,13 @@
+//
+//  HTACollectionViewLayout.m
+//  How To Adult
+//
+//  Created by Nathan Fennel on 2/23/17.
+//  Copyright © 2017 Nathan Fennel. All rights reserved.
+//
+
+#import "HTACollectionViewLayout.h"
+
+@implementation HTACollectionViewLayout
+
+@end

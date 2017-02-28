e18344bf5fe1276d583b1151ee4e0405b017fbdc
Nathan Fennel <nathanfennel@gmail.com>
2017-02-28 11:08:55 -0500

    Added Laundry Row Model

---
+++ How To Adult/HTALaundryRowModel.h
@@ -0,0 +1,13 @@
+//
+//  HTALaundryRowModel.h
+//  How To Adult
+//
+//  Created by Nathan Fennel on 2/28/17.
+//  Copyright © 2017 Nathan Fennel. All rights reserved.
+//
+
+#import "HTARowModel.h"
+
+@interface HTALaundryRowModel : HTARowModel
+
+@end
---
+++ How To Adult/HTALaundryRowModel.m
@@ -0,0 +1,13 @@
+//
+//  HTALaundryRowModel.m
+//  How To Adult
+//
+//  Created by Nathan Fennel on 2/28/17.
+//  Copyright © 2017 Nathan Fennel. All rights reserved.
+//
+
+#import "HTALaundryRowModel.h"
+
+@implementation HTALaundryRowModel
+
+@end

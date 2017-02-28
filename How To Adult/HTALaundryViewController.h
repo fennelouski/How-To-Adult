44d7bcf06631a04144b4095d8622b2c8c877e53c
Nathan Fennel <nathanfennel@gmail.com>
2017-02-28 11:11:00 -0500

    Added Laundry View Controller

---
+++ How To Adult/HTALaundryViewController.h
@@ -0,0 +1,13 @@
+//
+//  HTALaundryViewController.h
+//  How To Adult
+//
+//  Created by Nathan Fennel on 2/28/17.
+//  Copyright © 2017 Nathan Fennel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HTALaundryViewController : UIViewController
+
+@end
---
+++ How To Adult/HTALaundryViewController.m
@@ -0,0 +1,37 @@
+//
+//  HTALaundryViewController.m
+//  How To Adult
+//
+//  Created by Nathan Fennel on 2/28/17.
+//  Copyright © 2017 Nathan Fennel. All rights reserved.
+//
+
+#import "HTALaundryViewController.h"
+
+@interface HTALaundryViewController ()
+
+@end
+
+@implementation HTALaundryViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
